7c7dd790b5f6c4ee2b362cf3cd48cad2c74a9550
Kunnawuth Meebang <folknawat64@gmail.com>
2024-08-13 21:30:45 +0700

    comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from utils import air_pollution_data, update_air_pollution_data
-
+# from utils import air_pollution_data, update_air_pollution_data
+from utils import air_pollution_data
 # ข้อมูลการเข้าถึง API ในที่นี้ใช้ latitude = 13.7563, longitude = 100.5018 ของประเทศไทย
 token = "0148867a37d666e0e9d1202823b800fc"
 lat = "13.7563"
